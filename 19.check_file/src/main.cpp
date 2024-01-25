9fa82083dfd0cab92202598b33f44d1f07b380b2
dingyanxu <dingyanxu@dreame.tech>
2024-01-25 11:38:45 +0800

    修改生成文件大小

--- 19.check_file/src/main.cpp
+++ 19.check_file/src/main.cpp
@@ -3,11 +3,12 @@
 #include <vector>
 #include <getopt.h>
 
-const size_t ONE_GB = 1024 * 1024 * 1024;
+const uint64_t ONE_GB = 1024 * 1024 * 1024;
 const std::string OUT_FILE = "test.data";
+int g_testSize = 0;
 
 void createFile() {
-  std::cout << "creating 1GB file...." << std::endl;
+  std::cout << "creating " << g_testSize << "GB file...." << std::endl;
   std::ofstream file(OUT_FILE, std::ios::binary);
   if (!file) {
     std::cerr << "Cannot open file for writing." << std::endl;
@@ -15,19 +16,19 @@ void createFile() {
   }
 
   size_t writtenBytes = 0;
-  const size_t updateInterval = ONE_GB / 100;
+  const uint64_t updateInterval = g_testSize * ONE_GB / 100;
 
-  for (size_t i = 0; i < ONE_GB; ++i) {
+  for (size_t i = 0; i < g_testSize * ONE_GB; ++i) {
     char data = static_cast<char>(i % 256);
     file.write(&data, sizeof(data));
     ++writtenBytes;
 
     if (writtenBytes % updateInterval == 0) {
-      int progress = static_cast<int>((static_cast<double>(writtenBytes) / ONE_GB) * 100);
+      int progress = static_cast<int>((static_cast<double>(writtenBytes) / g_testSize * ONE_GB) * 100);
       std::cout << "\rProgress: " << progress << "%" << std::flush;  // Use \r to return to the beginning of the line
     }
   }
-  std::cout << "\nfile create completed!" << std::endl;
+  std::cout << "\nfile create completed! " << writtenBytes << std::endl;
 }
 
 int verifyFile(const std::string &toVerify) {
@@ -55,21 +56,22 @@ int verifyFile(const std::string &toVerify) {
 }
 
 int main(int argc, char *argv[]) {
-  const char *short_options = "cv:";
-  const struct option long_options[] = {{"create", no_argument, nullptr, 'c'},
+  const char *short_options = "c:v:";
+  const struct option long_options[] = {{"create", required_argument, nullptr, 'c'},
                                         {"verify", required_argument, nullptr, 'v'},
                                         {nullptr, 0, nullptr, 0}};
 
   int opt = 0;
-  std::string filename;
+  
 
   while ((opt = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1) {
     switch (opt) {
     case 'c':
+      g_testSize = std::stoi(optarg);
       createFile();
       return 0;
     case 'v': {
-      filename = optarg;
+      std::string filename = optarg;
       int errors = verifyFile(filename);
       std::cout << "Number of errors in file: " << errors << std::endl;
       return 0;
